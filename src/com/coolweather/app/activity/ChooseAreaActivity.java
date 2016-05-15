e403770535a400219d4f9963f67eef87be8472e3
DreamImpl <498577117@qq.com>
2016-05-15 22:26:51 +0800

    新增切换城市和手动更新天气的功能。

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -64,12 +64,18 @@ public class ChooseAreaActivity extends Activity {
 	*/
 	private int currentLevel;
 	
+	/**
+	* 是否从WeatherActivity中跳转过来。
+	*/
+	private boolean isFromWeatherActivity;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		if (prefs.getBoolean("city_selected", false)) {
+		// 已经选择了城市且不是从WeatherActivity跳转过来，才会直接跳转到WeatherActivity
+		if (prefs.getBoolean("city_selected", false) && !isFromWeatherActivity) {
 				Intent intent = new Intent(this, WeatherActivity.class);
 				startActivity(intent);
 				finish();
@@ -133,19 +139,19 @@ public class ChooseAreaActivity extends Activity {
 	* 查询选中省内所有的市，优先从数据库查询，如果没有查询到再去服务器上查询。
 	*/
 	private void queryCities() {
-	cityList = coolWeatherDB.loadCities(selectedProvince.getId());
-	if (cityList.size() > 0) {
-	dataList.clear();
-	for (City city : cityList) {
-	dataList.add(city.getCityName());
-	}
-	adapter.notifyDataSetChanged();
-	listView.setSelection(0);
-	titleText.setText(selectedProvince.getProvinceName());
-	currentLevel = LEVEL_CITY;
-	} else {
-	queryFromServer(selectedProvince.getProvinceCode(), "city");
-	}
+		cityList = coolWeatherDB.loadCities(selectedProvince.getId());
+		if (cityList.size() > 0) {
+			dataList.clear();
+			for (City city : cityList) {
+				dataList.add(city.getCityName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText(selectedProvince.getProvinceName());
+			currentLevel = LEVEL_CITY;
+		} else {
+			queryFromServer(selectedProvince.getProvinceCode(), "city");
+		}
 	}
 	
 	/**
@@ -256,6 +262,10 @@ public class ChooseAreaActivity extends Activity {
 		} else if (currentLevel == LEVEL_CITY) {
 			queryProvinces();
 		} else {
+			if(isFromWeatherActivity){
+				Intent intent = new Intent(this,WeatherActivity.class);
+				startActivity(intent);
+			}
 			finish();
 		}
 	}
